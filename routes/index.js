05ea7234d353fed8d66c289e6cc0a718caf3327e
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-05-13 15:42:18 +0100

    adding dashboard page: - list of applied roles section - styling

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var router = new express.Router();
 /* GET home page. */
 router.get('/', function index(req, res) {
   res.render('index', {
-    title: 'Express'
+    roles: [
+      { title: 'Graphic designer', employer: 'Department of Work and Pensions', status: 'Applied' },
+      { title: 'UX designer', employer: 'Facebook', status: 'Interview Scheduled' },
+      { title: 'Graphic designer', employer: 'Apple', status: 'No longer interested' }
+    ]
   });
 });
 

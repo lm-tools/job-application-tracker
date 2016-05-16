b7d26c3d4c48271dc06731e27e5556a471004982
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-05-16 12:57:15 +0100

    displaying states

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,54 @@ var jobApplicationStates = require('../lib/job_application_states');
 var router = new express.Router();
 var Stately = require('stately.js');
 
+
+var RolesView = function (roles) {
+  function formatStateQuestion(state) {
+    return _.capitalize(state.replace(/_/g, ' ')) + ' ?';
+  }
+
+  function lookupStatus(state) {
+    switch (state) {
+      case 'HAVE_YOU_DONE_EVERYTHING_YOU_NEED_TO_APPLY':
+      case 'ARE_YOU_STILL_INTERESTED_IN_PREPARING_TO_APPLYING':
+      case 'HAVE_YOU_APPLIED':
+      case 'ARE_YOU_STILL_INTERESTED_IN_APPLYING':
+        return 'Interested';
+
+      case 'HAVE_YOU_HEARD_BACK_YET':
+      case 'ARE_YOU_STILL_INTERESTED_IN_HEARING_BACK':
+      case 'HAVE_YOU_TRIED_FOLLOWING_UP_ABOUT_HEADING_BACK':
+      case 'HAVE_YOU_SCHEDULED_AN_INTERVIEW':
+      case 'ARE_YOU_STILL_INTERESTED_IN_SCHEDULING_AN_INTERVIEW':
+      case 'HAVE_YOU_TRIED_FOLLOWING_UP_ABOUT_SCHEDULING_AN_INTERVIEW':
+        return 'Applied';
+
+      case 'ARE_YOU_READY_FOR_YOUR_INTERVIEW':
+      case 'HAVE_YOU_INTERVIEWED_YET':
+      case 'HAVE_YOU_RECEIVED_AN_OFFER':
+      case 'ARE_YOU_STILL_INTERESTED_IN_RECEIVING_AN_OFFER':
+      case 'HAVE_YOU_TRIED_FOLLOWING_UP_ABOUT_RECEIVING_AN_OFFER':
+        return 'Interviewing';
+
+      case 'SUCCEEDED':
+      case 'ABANDONED':
+      default:
+        return '';
+    }
+  }
+
+  return _.map(roles, function (role) {
+    return _.extend({
+      stateQuestion: formatStateQuestion(role.state),
+      status: lookupStatus(role.state)
+    }, role);
+  });
+};
+
 /* GET home page. */
 router.get('/', function index(req, res) {
   res.render('index', {
-    roles: req.session.roles
+    roles: new RolesView(req.session.roles)
   });
 });
 
